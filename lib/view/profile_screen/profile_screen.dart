e89bf5f542a81760e8f53b9e034c22aa98dd76e4
ABY4613 <abyb4613@gmail.com>
2024-12-14 22:54:46 +0530

    first commit

---
+++ lib/view/profile_screen/profile_screen.dart
@@ -0,0 +1,50 @@
+
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:news_api/utilts/colors_const.dart';
+import 'package:news_api/view/login_screen/login_screen.dart';
+
+
+import 'package:shared_preferences/shared_preferences.dart';
+
+
+class ProfileScreen extends StatefulWidget {
+  const ProfileScreen({super.key});
+
+  @override
+  State<ProfileScreen> createState() => _BottomNavbarScreenState();
+}
+
+class _BottomNavbarScreenState extends State<ProfileScreen> {
+    int selectindex=0;
+
+  @override
+  Widget build(BuildContext context) {
+    
+     return Scaffold(
+      
+     appBar: AppBar(title: Text("home screen"),),
+     body:  Column(
+       children: [
+       // Text("$saved")
+        ElevatedButton(
+          style:  ElevatedButton.styleFrom(
+            backgroundColor:ColorConstants.PRIMARY
+                      ),
+                  onPressed: () async { 
+                    SharedPreferences prefs=await SharedPreferences.getInstance();
+                    await prefs.remove('username');
+                    await prefs.remove('password');
+                   Navigator.pushReplacement( 
+                      context,
+                      MaterialPageRoute(
+                        builder:  (context) => LoginScreen(),)
+                       );
+                },
+                 child: Text("Logout")),
+       ],
+     ),
+    );
+  }
+}
---
+++ lib/view/welcome_screen/welcome_screen.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:news_api/utilts/colors_const.dart';
+import 'package:news_api/utilts/images_const.dart';
+import 'package:news_api/view/login_screen/login_screen.dart';
+import 'package:news_api/view/welcome_screen/welcome_screen.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: Padding(
+          padding: const EdgeInsets.only(left: 17),
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                "1",
+                style: GoogleFonts.montserrat(
+                    color: ColorConstants.BLACK,
+                    fontWeight: FontWeight.w600,
+                    fontSize: 18),
+              ),
+              Text(
+                "/3",
+                style: GoogleFonts.montserrat(
+                    color: ColorConstants.GREYSHDE3,
+                    fontWeight: FontWeight.w600,
+                    fontSize: 18),
+              ),
+            ],
+          ),
+        ),
+        actions: [
+          InkWell(
+            onTap: () {
+              Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => LoginScreen(),
+                  ));
+            },
+            child: Padding(
+              padding: const EdgeInsets.only(right: 17.0),
+              child: Text(
+                "Skip",
+                style: GoogleFonts.montserrat(
+                    color: ColorConstants.BLACK,
+                    fontWeight: FontWeight.w600,
+                    fontSize: 18),
+              ),
+            ),
+          ),
+        ],
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 18, vertical: 22),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                ImageConstants.MYAPPLOGO,
+                height: 300,
+                width: 300,
+              ),
+              SizedBox(height: 15),
+              Text(
+                "Choose Products",
+                style: GoogleFonts.montserrat(
+                    color: ColorConstants.BLACK,
+                    fontWeight: FontWeight.w900,
+                    fontSize: 24),
+              ),
+              SizedBox(height: 10),
+              Text(
+                "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit.",
+                textAlign: TextAlign.center,
+                style: GoogleFonts.montserrat(
+                    color: ColorConstants.GREYSHDE1,
+                    fontWeight: FontWeight.w600,
+                    fontSize: 14),
+              ),
+            ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 19, vertical: 24),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              "Prev",
+              style: GoogleFonts.montserrat(
+                  color: ColorConstants.GREYSHDE2,
+                  fontWeight: FontWeight.w600,
+                  fontSize: 18),
+            ),
+            Text(
+              "Next",
+              style: GoogleFonts.montserrat(
+                  color: ColorConstants.PRIMARY,
+                  fontWeight: FontWeight.w600,
+                  fontSize: 18),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
